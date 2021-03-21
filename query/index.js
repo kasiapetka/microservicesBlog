e3a00aa353be5638c275e9c84064dafa458be3a0
kasiapetka <katarzynapetka7@gmail,com>
2021-03-21 13:52:56 +0100

    moderation service

--- query/index.js
+++ query/index.js
@@ -22,13 +22,22 @@ app.post('/events', (req, res) => {
   }
 
   if (type === 'CommentCreated') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
-  console.log(posts);
+  if (type === 'CommentUpdated') {
+    const { id, postId, status, content } = data;
+    const comments = posts[postId].comments;
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    })
+    comment.status = status;
+    comment.content = content;
+  }
 
   res.send({});
 });
